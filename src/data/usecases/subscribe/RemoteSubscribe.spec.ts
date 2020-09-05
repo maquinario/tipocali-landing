05e5b817b2fc098b244a314499a85bf9f5e0c5a0
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-05 20:59:33 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/subscribe/RemoteSubscribe.spec.ts
+++ src/data/usecases/subscribe/RemoteSubscribe.spec.ts
@@ -5,16 +5,18 @@ import { HttpPostClientSpy } from '@/data/test/MockHttpClient'
 import { mockSubscribe } from '@/domain/test/MockSubscribe'
 import { HttpStatusCode } from '@/data/protocols/http/HttpResponse'
 import { UnexpectedError } from '@/domain/errors/UnexpectedError'
+import { SubscribeParams } from '@/domain/usecases/Subscribe'
+import { SubscriberModel } from '@/domain/models/SubscriberModel'
 
 type SutTypes = {
   sut: RemoteSubscribe
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<SubscribeParams, SubscriberModel>
 }
 
 const url = faker.internet.url()
 
 const makeSut = (): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<SubscribeParams, SubscriberModel>()
   const sut = new RemoteSubscribe(url, httpPostClientSpy)
 
   return { sut, httpPostClientSpy}
@@ -44,6 +46,14 @@ describe('RemoteSubscribe', () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
       statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.subscribe(mockSubscribe())
+    expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+  test('Should throw ServerError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
     }
     const promise = sut.subscribe(mockSubscribe())
     expect(promise).rejects.toThrow(new UnexpectedError())
--- src/data/usecases/subscribe/RemoteSubscribe.ts
+++ src/data/usecases/subscribe/RemoteSubscribe.ts
@@ -2,11 +2,12 @@ import { HttpPostClient } from '@/data/protocols/http/HttpPostClient'
 import { SubscribeParams } from '@/domain/usecases/Subscribe'
 import { HttpStatusCode } from '@/data/protocols/http/HttpResponse'
 import { UnexpectedError } from '@/domain/errors/UnexpectedError'
+import { SubscriberModel } from '@/domain/models/SubscriberModel'
 
 export class RemoteSubscribe {
   constructor (
     private readonly url: string,
-    private readonly httpPostClient: HttpPostClient
+    private readonly httpPostClient: HttpPostClient<SubscribeParams, SubscriberModel>
   ) {}
 
   async subscribe (params: SubscribeParams): Promise<void> {
