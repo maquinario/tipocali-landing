ad62577a3dd188e39f60497115fd49d3574237f6
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-05 20:55:02 -0300

    test: ensure RemoteSubscribe throws UnexpectedError on default cases

--- src/data/usecases/subscribe/RemoteSubscribe.spec.ts
+++ src/data/usecases/subscribe/RemoteSubscribe.spec.ts
@@ -40,4 +40,20 @@ describe('RemoteSubscribe', () => {
     const promise = sut.subscribe(mockSubscribe())
     expect(promise).rejects.toThrow(new UnexpectedError())
   })
+  test('Should throw ServerError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.subscribe(mockSubscribe())
+    expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+  test('Should throw ServerError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.subscribe(mockSubscribe())
+    expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
