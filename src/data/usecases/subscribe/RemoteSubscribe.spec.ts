8d40c0e91476994ed99dfd61b5a04d16173548d1
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-05 20:12:49 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/subscribe/RemoteSubscribe.spec.ts
+++ src/data/usecases/subscribe/RemoteSubscribe.spec.ts
@@ -3,11 +3,23 @@ import { HttpPostClient } from '../../protocols/http/HttpPostClient'
 import { RemoteSubscribe } from './RemoteSubscribe'
 import { HttpPostClientSpy } from '@/data/test/MockHttpClient'
 
+type SutTypes = {
+  sut: RemoteSubscribe
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const url = faker.internet.url()
+
+const makeSut = (): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteSubscribe(url, httpPostClientSpy)
+
+  return { sut, httpPostClientSpy}
+}
+
 describe('RemoteSubscribe', () => {
   test('Should call HttpPostClient with correct url', async () => {
-    const url = faker.internet.url()
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteSubscribe(url, httpPostClientSpy)
+    const { sut, httpPostClientSpy } = makeSut()
     await sut.subscribe()
     expect(httpPostClientSpy.url).toBe(url)
   })
