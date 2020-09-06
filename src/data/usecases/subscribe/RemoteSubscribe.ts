651ff3708b752f8e3f3689e3696a3ea04dacdb32
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-05 21:06:02 -0300

    feat: ensure RemoteSubscribe returns a Subscriber on success

--- src/data/usecases/subscribe/RemoteSubscribe.ts
+++ src/data/usecases/subscribe/RemoteSubscribe.ts
@@ -1,22 +1,22 @@
 import { HttpPostClient } from '@/data/protocols/http/HttpPostClient'
-import { SubscribeParams } from '@/domain/usecases/Subscribe'
+import { SubscribeParams, Subscribe } from '@/domain/usecases/Subscribe'
 import { HttpStatusCode } from '@/data/protocols/http/HttpResponse'
 import { UnexpectedError } from '@/domain/errors/UnexpectedError'
 import { SubscriberModel } from '@/domain/models/SubscriberModel'
 
-export class RemoteSubscribe {
+export class RemoteSubscribe implements Subscribe {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<SubscribeParams, SubscriberModel>
   ) {}
 
-  async subscribe (params: SubscribeParams): Promise<void> {
+  async subscribe (params: SubscribeParams): Promise<SubscriberModel> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
     switch(httpResponse.statusCode){
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       default: throw new UnexpectedError()
     }
   }
