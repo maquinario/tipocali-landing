b68c965ad36b1877bbc405c9d185225593426814
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-06 20:31:33 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/Subscribe/Subscribe.spec.tsx
+++ src/presentation/pages/Subscribe/Subscribe.spec.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Subscribe from '.'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Subscribe />)
+  return { sut }
+}
 
 describe('Subscribe Component', () => {
   test('Button should be disabled and not be loading', () => {
-    const { getByRole } = render(<Subscribe />)
-    const button = getByRole('submit') as HTMLButtonElement
+    const { sut } = makeSut()
+    const button = sut.getByRole('submit') as HTMLButtonElement
     expect(button.classList).not.toContain('loading')
     expect(button.disabled).toBe(true)
   })
   test('Errors should not be displayed on initial state', () => {
-    const { getByRole } = render(<Subscribe />)
-    const errorContainer = getByRole('errors')
+    const { sut } = makeSut()
+    const errorContainer = sut.getByRole('errors')
     expect(errorContainer.childElementCount).toBe(0)
   })
 })
