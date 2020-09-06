4b1b1cc35781ed2899c2b1db59959899300f0846
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-06 07:49:23 -0300

    test: ensure button is disabled on initial state

--- src/presentation/pages/Subscribe/Subscribe.spec.tsx
+++ src/presentation/pages/Subscribe/Subscribe.spec.tsx
@@ -3,9 +3,10 @@ import { render } from '@testing-library/react'
 import Subscribe from '.'
 
 describe('Subscribe Component', () => {
-  test('Button should not be loading', () => {
+  test('Button should be disabled and not be loading', () => {
     const { getByRole } = render(<Subscribe />)
-    const button = getByRole('submit')
+    const button = getByRole('submit') as HTMLButtonElement
     expect(button.classList).not.toContain('loading')
+    expect(button.disabled).toBe(true)
   })
 })
