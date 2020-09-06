880901a690ab5dd804bea7218fd56e4098e5ae89
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-06 07:44:43 -0300

    feat: ensure Subscribe Button does not have loading class

--- src/presentation/pages/Subscribe/index.tsx
+++ src/presentation/pages/Subscribe/index.tsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 
 const Subscribe: React.FC = () => {
+  const [loading] = useState<Boolean>(false)
+  const [valid] = useState<Boolean>(false)
+  const [errorMessage] = useState<string>(null)
+
+  const handleSubmit = useCallback(() => {
+    event.preventDefault()
+  }, [valid])
+
   return (
     <div className="subscribe">
       <div className="subscribe-container">
-        <h1 className="subscribe-title">Workshop - Apaixonados por Caligrafia</h1>
-        <h3 className="subscribe-subtitle">Inscreva-se aqui para participar do meu workshop online</h3>
+        <h1 className="subscribe-title">
+          Workshop - Apaixonados por Caligrafia
+        </h1>
+        <h3 className="subscribe-subtitle">
+          Inscreva-se aqui para participar do meu workshop online
+        </h3>
         <div className="subscribe-text">
-          <p>Deixe seu nome e e-mail  para garantir sua participação</p>
+          <p>Deixe seu nome e e-mail para garantir sua participação</p>
         </div>
-        <form action="#" className="subscribe-form">
-          <input type="text" placeholder="Nome" className="form-field field-name"/>
-          <input type="email" placeholder="Email" className="form-field field-email"/>
-          <button type="submit">Cadastrar</button>
+        <form onSubmit={handleSubmit} className="subscribe-form">
+          <div className="subscribe-fields">
+            <input
+              type="text"
+              placeholder="Nome"
+              className="form-field field-name"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="form-field field-email"
+            />
+            {errorMessage && errorMessage}
+          </div>
+          <button
+            role="submit"
+            type="submit"
+            disabled={!valid}
+            className={`submitBtn ${loading && 'loading'}`}
+          >
+            Cadastrar
+          </button>
         </form>
       </div>
     </div>
