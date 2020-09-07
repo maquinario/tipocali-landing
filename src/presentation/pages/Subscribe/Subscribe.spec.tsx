d3e08cfdf8f65350aa996ab136a9887120d54f47
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-07 02:42:53 -0300

    test: ensure Subscribe calls validation with correct email

--- src/presentation/pages/Subscribe/Subscribe.spec.tsx
+++ src/presentation/pages/Subscribe/Subscribe.spec.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import Subscribe from '.'
+import { Validation } from '@/presentation/protocols/Validation'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Subscribe />)
-  return { sut }
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Subscribe validation={validationSpy} />)
+  return { sut, validationSpy }
 }
 
 describe('Subscribe Component', () => {
+  afterEach(cleanup)
+
   test('Button should be disabled and not be loading', () => {
     const { sut } = makeSut()
     const button = sut.getByRole('submit') as HTMLButtonElement
@@ -22,5 +36,13 @@ describe('Subscribe Component', () => {
     const { sut } = makeSut()
     const errorContainer = sut.getByRole('errors')
     expect(errorContainer.childElementCount).toBe(0)
+  })
+  test('Errors call validation with correct email', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByPlaceholderText('Email')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
   })
 })
