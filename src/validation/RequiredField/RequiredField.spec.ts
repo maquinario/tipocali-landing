fa7d9e57b91141551991f5a63d9987b2068afdd0
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-07 21:24:24 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/RequiredField/RequiredField.spec.ts
+++ src/validation/RequiredField/RequiredField.spec.ts
@@ -1,3 +1,4 @@
+import faker from 'faker'
 import { RequiredFieldValidation } from '.'
 import { RequiredFieldError } from '../errors/RequiredFieldError'
 
@@ -6,5 +7,10 @@ describe('RequiredFieldValidation', () => {
     const sut = new RequiredFieldValidation('name')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('name')
+    const error = sut.validate(faker.random.words())
+    expect(error).toBeFalsy()
   })
 })
