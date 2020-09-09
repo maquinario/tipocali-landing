36cb5d5d9a227d4c7e9d3c64dccc4ff439273be9
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-09 19:19:25 -0300

    test: ensure ValidationBuilder has an email helper

--- src/validation/validators/Builder/ValidationBuilder.spec.ts
+++ src/validation/validators/Builder/ValidationBuilder.spec.ts
@@ -1,5 +1,5 @@
 import faker from 'faker'
-import { RequiredFieldValidation } from '@/validation/validators'
+import { RequiredFieldValidation, EmailValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from '.'
 
 describe('ValidationBuilder', () => {
@@ -7,5 +7,10 @@ describe('ValidationBuilder', () => {
     const fieldName = faker.database.column()
     const validations = sut.field(fieldName).required().build()
     expect(validations).toEqual([new RequiredFieldValidation(fieldName)])
+  })
+  test('Sould return EmailValidation', () => {
+    const fieldName = faker.database.column()
+    const validations = sut.field(fieldName).required().build()
+    expect(validations).toEqual([new EmailValidation(fieldName)])
   })
 })
