1b4c1ae5866214b5781b69ae9a1dcd0869168008
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-09 19:07:45 -0300

    refactor: move sut creation to a factory method

--- src/validation/validators/ValidationComposite/ValidationComposite.spec.ts
+++ src/validation/validators/ValidationComposite/ValidationComposite.spec.ts
@@ -2,15 +2,27 @@ import faker from 'faker'
 import { ValidationComposite } from '.'
 import { FieldValidationSpy } from '../test/MockFieldValidation'
 
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationSpies: FieldValidationSpy[]
+}
+
+const fieldName = faker.database.column()
+const makeSut = (): SutTypes => {
+  const fieldValidationSpies = [
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
+  ]
+  const sut = new ValidationComposite(fieldValidationSpies)
+
+  return { sut, fieldValidationSpies }
+}
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const fieldName = faker.database.column()
+    const { sut, fieldValidationSpies } = makeSut()
     const errorMessage = faker.random.words(3)
-    const fieldValidationSpy = new FieldValidationSpy(fieldName)
-    fieldValidationSpy.error = new Error(errorMessage)
-    const fieldValidationSpy2 = new FieldValidationSpy(fieldName)
-    fieldValidationSpy2.error = new Error(faker.random.words(3))
-    const sut = new ValidationComposite([fieldValidationSpy, fieldValidationSpy2])
+    fieldValidationSpies[0].error = new Error(errorMessage)
+    fieldValidationSpies[1].error = new Error(faker.random.words(3))
     const error = sut.validate(fieldName, faker.random.word())
     expect(error).toBe(errorMessage)
   })
