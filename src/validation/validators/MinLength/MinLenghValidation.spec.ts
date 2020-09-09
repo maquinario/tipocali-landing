099b8161b6e854d8810a2364448c8805d1910b9e
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-09 18:46:54 -0300

    test: ensure MinLenghValidation returns error if value is invalid

---
+++ src/validation/validators/MinLength/MinLenghValidation.spec.ts
@@ -0,0 +1,11 @@
+import faker from 'faker'
+import { InvalidFieldError } from '@/validation/errors'
+import { MinLenghValidation } from '.'
+
+describe('MinLenghValidation', () => {
+  test('Should return error if value is invalid', () => {
+    const sut = new MinLenghValidation(faker.database.column(), 5)
+    const error = sut.validate(faker.random.alphaNumeric(3))
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
