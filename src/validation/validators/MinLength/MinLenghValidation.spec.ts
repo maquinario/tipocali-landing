6f5c5a740401eaf97784e7b118a37dd18609d0be
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-09 18:49:23 -0300

    test: ensure MinLenghValidation returns fasly if value is valid

--- src/validation/validators/MinLength/MinLenghValidation.spec.ts
+++ src/validation/validators/MinLength/MinLenghValidation.spec.ts
@@ -8,4 +8,9 @@ describe('MinLenghValidation', () => {
     const error = sut.validate(faker.random.alphaNumeric(3))
     expect(error).toEqual(new InvalidFieldError())
   })
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLenghValidation(faker.database.column(), 5)
+    const error = sut.validate(faker.random.alphaNumeric(8))
+    expect(error).toBeFalsy()
+  })
 })
