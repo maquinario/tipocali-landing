0afe50b32e8d0958bea07e3885e101a53e88616c
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-09 18:53:10 -0300

    feat: ensure EmailValidation allows empty values

--- src/validation/validators/Email/index.ts
+++ src/validation/validators/Email/index.ts
@@ -6,6 +6,6 @@ export class EmailValidation implements FieldValidation {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
   validate (value: string): Error {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return emailRegex.test(value) ? null : new InvalidFieldError()
+    return (!value || emailRegex.test(value)) ? null : new InvalidFieldError()
   }
 }
