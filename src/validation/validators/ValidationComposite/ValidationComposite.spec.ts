d2adb2853cc770b548881da57b7b2e56cafd0f59
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-09 19:02:46 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/ValidationComposite/ValidationComposite.spec.ts
@@ -0,0 +1,16 @@
+import faker from 'faker'
+import { ValidationComposite } from '.'
+import { FieldValidationSpy } from '../test/MockFieldValidation'
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', () => {
+    const fieldName = faker.database.column()
+    const errorMessage = faker.random.words(3)
+    const fieldValidationSpy = new FieldValidationSpy(fieldName)
+    const fieldValidationSpy2 = new FieldValidationSpy(fieldName)
+    fieldValidationSpy2.error = new Error(errorMessage)
+    const sut = new ValidationComposite([fieldValidationSpy, fieldValidationSpy2])
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(errorMessage)
+  })
+})
