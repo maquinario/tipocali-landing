274091bf9b38d5b8a1c978a4d7af4ca55c9e76a8
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-09 19:02:32 -0300

    feat: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/test/MockFieldValidation.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars-experimental */
+import { FieldValidation } from '@/validation/protocols'
+
+export class FieldValidationSpy implements FieldValidation {
+  error: Error = null
+  constructor (readonly field: string) {}
+  validate (value: string): Error {
+    return this.error
+  }
+}
